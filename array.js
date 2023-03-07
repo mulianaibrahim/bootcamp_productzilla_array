a4b44cef3e00c8900040c7525c8f52c88398234f
FIRST_NAME LAST_NAME <mulianaibrahim@gmail.com>
2023-03-07 20:39:31 +0700

    inisiasi project

---
+++ array.js
@@ -0,0 +1,18 @@
+let anggota = ['muliana', 'anisa', 'fahran', 'rayhan', 'indah', 'farah'];
+let bidang = ['adm', 'konsumsi', 'acara', 'dekorasi', 'dokumentasi', 'keamanan'];
+let jurusan = ['sistem informasi', 'teknik informatika', 'teknik elektro', 'teknik mesin', 'teknik kimia'];
+
+console.log(anggota[1]);
+console.log(anggota[3]);
+console.log(anggota[5]);
+console.log(anggota.length)
+
+console.log(bidang[1]);
+console.log(bidang[3]);
+console.log(bidang[5]);
+console.log(bidang.length)
+
+console.log(jurusan[1]);
+console.log(jurusan[3]);
+console.log(jurusan[5]);
+console.log(jurusan.length)
